ab6f2a64079ac095a55b7edc8bdfa2b05983c470
gdebustamante <germanbustamante.trabajo@gmail.com>
2022-01-13 14:07:30 +0100

    Mejorado API

--- CRUD_Personas_API/Controllers/PersonasController.cs
+++ CRUD_Personas_API/Controllers/PersonasController.cs
@@ -2,7 +2,16 @@
 using CRUD_Personas_BL.Manejadoras;
 using CRUD_Personas_Entidades;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using FromBodyAttribute = Microsoft.AspNetCore.Mvc.FromBodyAttribute;
+using HttpDeleteAttribute = Microsoft.AspNetCore.Mvc.HttpDeleteAttribute;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+using HttpPutAttribute = Microsoft.AspNetCore.Mvc.HttpPutAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,7 +25,22 @@ namespace CRUD_Personas_API.Controllers
         [HttpGet]
         public IEnumerable<ClsPersona> Get()
         {
-            return ClsListadoPersonasBL.getListadoPersonasCompletoBL(); ;
+            List<ClsPersona> listaPersonas;
+            try
+            {
+                listaPersonas = new List<ClsPersona>(ClsListadoPersonasBL.getListadoPersonasCompletoBL());
+            }
+            catch (Exception e)
+            {
+                throw new HttpResponseException(HttpStatusCode.ServiceUnavailable);
+            }
+
+            if (listaPersonas == null || listaPersonas.Count == 0)
+            {
+                throw new HttpResponseException(HttpStatusCode.NoContent);
+            }
+
+            return ClsListadoPersonasBL.getListadoPersonasCompletoBL();
         }
 
         // GET api/<PersonasController>/5
@@ -28,7 +52,7 @@ namespace CRUD_Personas_API.Controllers
 
         // POST api/<PersonasController>
         [HttpPost]
-        public void Post([FromBody] ClsPersona oPersona)
+        public void Post([FromBody] ClsPersona oPersona)//[FromBody] 
         {
             ClsManejadoraPersonaBL.actualizarAñadirPersonaBL(oPersona);
         }
